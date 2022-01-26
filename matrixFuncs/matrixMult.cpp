4ee724f031a6131e945b85c90e0395af6728a5f4
Kabir Singh <sahai.kabir1@gmail.com>
2022-01-26 09:51:19 -0700

    Multiply Matrices

--- matrixFuncs/matrixMult.cpp
+++ matrixFuncs/matrixMult.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include "../linker.h"
 using namespace std;
+
+void multExt(float inputArr[5][5], int multiplier){
+  for(int i=0; i<5; i++){
+    cout << "[";
+    for(int j=0; j<5; j++){
+      inputArr[i][j] = inputArr[i][j] * multiplier;
+      cout << inputArr[i][j] << ", ";
+	  }
+    cout << "]" << endl;
+  }
+}
