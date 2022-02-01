a7428a5c00b1d7f03e9ca9a28d92a7126129f628
Kabir Singh <sahai.kabir1@gmail.com>
2022-02-01 00:01:56 -0700

    gtg sleep screen time

--- matrixFuncs/matrixToMatrixMult.cpp
+++ matrixFuncs/matrixToMatrixMult.cpp
@@ -3,9 +3,20 @@
 using namespace std;
 
 void multTwoMatricesExt(double inputArr[5][5], double inputArrMult[5][5]){
+  double newArr[5][5] = {
+      {0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0}
+  };
   for(int i=0; i<5; i++){
-    for(int j=0; j<5; j++){
-      inputArr[i][j] = inputArr[i][j] * inputArrMult[j][i];
-	}
+      for(int j=0; j<5; j++){
+          newArr[i][j] = 0;
+          for(int k=0; k<5; k++){
+              newArr[i][j] += inputArrMult[i][k] * inputArr[k][j];
+          }
+      }
   }
+  inputArr = newArr;
 }
